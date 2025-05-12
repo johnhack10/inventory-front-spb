b02264e4fe05e577a49d64ca626482549e1fac4a
John Arango <jfamdevelop@gmail.com>
2025-05-12 12:25:45 -0500

    Integración con Keycloak

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,30 @@
-import { ApplicationConfig } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig } from '@angular/core';
 import { PreloadAllModules, provideRouter, withComponentInputBinding, withHashLocation, withPreloading } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { KeycloakService } from 'keycloak-angular';
+
+
+function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+    keycloak.init({
+      config: {
+        url: 'http://localhost:8082',
+        realm: 'inventory-realm',
+        clientId: 'angular-client-inventory'
+      },
+      initOptions: {
+        onLoad: 'login-required',
+        flow: 'standard',
+        silentCheckSsoRedirectUri:
+          window.location.origin + '/assets/silent-check-sso.html'
+      },
+      loadUserProfileAtStartUp: true,
+    });
+}
+
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes,
@@ -11,6 +32,13 @@ export const appConfig: ApplicationConfig = {
     withHashLocation(),
     withPreloading(PreloadAllModules)),
     provideHttpClient(),
-    provideAnimationsAsync()
+    provideAnimationsAsync(),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      deps: [KeycloakService],
+      multi: true
+    },
+    KeycloakService,
   ]
 };
